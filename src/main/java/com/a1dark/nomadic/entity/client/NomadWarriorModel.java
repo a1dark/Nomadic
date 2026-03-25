4884d3c0ffd31230befe9f66abae3a0e42c07ef0
Aidar Kubanychbekov <aidarstudent24@gmail.com>
2026-03-25 21:15:42 +0100

    Added archer variant for a custom entity

--- src/main/java/com/a1dark/nomadic/entity/client/NomadWarriorModel.java
+++ src/main/java/com/a1dark/nomadic/entity/client/NomadWarriorModel.java
@@ -4,19 +4,18 @@ import com.a1dark.nomadic.Nomadic;
 import com.a1dark.nomadic.entity.custom.NomadWarriorEntity;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
-import net.minecraft.client.model.AnimationUtils;
 import net.minecraft.client.model.ArmedModel;
-import net.minecraft.client.model.EntityModel;
+import net.minecraft.client.model.HierarchicalModel;
 import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.model.geom.PartPose;
 import net.minecraft.client.model.geom.builders.*;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.util.Mth;
 import net.minecraft.world.entity.HumanoidArm;
+import net.minecraft.client.model.AnimationUtils;
 
 public class NomadWarriorModel<T extends NomadWarriorEntity>
-        extends EntityModel<T> implements ArmedModel {
+        extends HierarchicalModel<T> implements ArmedModel {
 
     public static final ModelLayerLocation LAYER_LOCATION =
             new ModelLayerLocation(
@@ -30,14 +29,16 @@ public class NomadWarriorModel<T extends NomadWarriorEntity>
     private final ModelPart head;
     private final ModelPart rightArm;
     private final ModelPart leftArm;
+    private final ModelPart hat;
     private final ModelPart rightLeg;
     private final ModelPart leftLeg;
 
     public NomadWarriorModel(ModelPart root) {
-        this.root = root;
-        this.waist = root.getChild("waist");
+        this.root = root.getChild("waist");
+        this.waist = this.root;
         this.body = waist.getChild("body");
         this.head = body.getChild("head");
+        this.hat = head.getChild("hat");
         this.rightArm = body.getChild("rightArm");
         this.leftArm = body.getChild("leftArm");
         this.rightLeg = body.getChild("rightLeg");
@@ -48,49 +49,37 @@ public class NomadWarriorModel<T extends NomadWarriorEntity>
         MeshDefinition meshdefinition = new MeshDefinition();
         PartDefinition partdefinition = meshdefinition.getRoot();
 
-        PartDefinition waist = partdefinition.addOrReplaceChild("waist",
-                CubeListBuilder.create(),
-                PartPose.offset(0.0F, 12.0F, 0.0F));
+        PartDefinition waist = partdefinition.addOrReplaceChild("waist", CubeListBuilder.create(), PartPose.offset(0.0F, 24.0F, 0.0F));
 
-        PartDefinition body = waist.addOrReplaceChild("body",
-                CubeListBuilder.create()
-                        .texOffs(0, 32)
-                        .addBox(-4.0F, -3.0F, -2.0F, 8.0F, 15.0F, 4.0F),
-                PartPose.offset(0.0F, -12.0F, 0.0F));
+        PartDefinition body = waist.addOrReplaceChild("body", CubeListBuilder.create().texOffs(26, 35).addBox(-4.0F, -1.0F, -3.0F, 8.0F, 9.0F, 6.0F, new CubeDeformation(0.0F))
+                .texOffs(0, 32).addBox(-4.0F, -3.0F, -2.0F, 8.0F, 15.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -24.0F, 0.0F));
 
-        PartDefinition head = body.addOrReplaceChild("head",
-                CubeListBuilder.create()
-                        .texOffs(0, 0)
-                        .addBox(-4.0F, -11.0F, -4.0F, 8.0F, 8.0F, 8.0F),
-                PartPose.offset(0.0F, 0.0F, 0.0F));
+        PartDefinition head = body.addOrReplaceChild("head", CubeListBuilder.create().texOffs(0, 0).addBox(-4.0F, -11.0F, -4.0F, 8.0F, 8.0F, 8.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
 
-        body.addOrReplaceChild("rightArm",
-                CubeListBuilder.create()
-                        .texOffs(32, 0)
-                        .addBox(-2.0F, -1.0F, -2.0F, 4.0F, 15.0F, 4.0F),
-                PartPose.offset(-6.0F, -2.0F, 0.0F));
+        PartDefinition hat = head.addOrReplaceChild("hat", CubeListBuilder.create().texOffs(0, 16).addBox(-4.0F, -11.0F, -4.0F, 8.0F, 8.0F, 8.0F, new CubeDeformation(0.5F))
+                .texOffs(34, 47).addBox(0.0F, -15.0F, -1.0F, 0.0F, 5.0F, 7.0F, new CubeDeformation(0.0F))
+                .texOffs(41, 52).addBox(-5.0F, -10.0F, -5.0F, 1.0F, 1.0F, 10.0F, new CubeDeformation(0.0F))
+                .texOffs(41, 52).addBox(4.0F, -10.0F, -5.0F, 1.0F, 1.0F, 10.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
 
-        body.addOrReplaceChild("leftArm",
-                CubeListBuilder.create()
-                        .texOffs(32, 0)
-                        .mirror()
-                        .addBox(-2.0F, -1.0F, -2.0F, 4.0F, 15.0F, 4.0F)
-                        .mirror(false),
-                PartPose.offset(6.0F, -2.0F, 0.0F));
+        PartDefinition cube_r1 = hat.addOrReplaceChild("cube_r1", CubeListBuilder.create().texOffs(41, 52).addBox(-1.0F, -16.0F, -8.0F, 1.0F, 1.0F, 10.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-3.0F, 6.0F, 5.0F, 0.0F, -1.5708F, 0.0F));
 
-        body.addOrReplaceChild("rightLeg",
-                CubeListBuilder.create()
-                        .texOffs(32, 19)
-                        .addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F),
-                PartPose.offset(-2.0F, 12.0F, 0.0F));
+        PartDefinition cube_r2 = hat.addOrReplaceChild("cube_r2", CubeListBuilder.create().texOffs(41, 52).addBox(-1.0F, -16.0F, -8.0F, 1.0F, 1.0F, 10.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-3.0F, 6.0F, -4.0F, 0.0F, -1.5708F, 0.0F));
 
-        body.addOrReplaceChild("leftLeg",
-                CubeListBuilder.create()
-                        .texOffs(32, 19)
-                        .mirror()
-                        .addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F)
-                        .mirror(false),
-                PartPose.offset(2.0F, 12.0F, 0.0F));
+        PartDefinition rightArm = body.addOrReplaceChild("rightArm", CubeListBuilder.create().texOffs(32, 0).addBox(-2.0F, -0.75F, -3.4749F, 4.0F, 15.0F, 4.0F, new CubeDeformation(0.0F))
+                .texOffs(13, 54).addBox(-3.0F, -1.0F, -4.0F, 4.0F, 4.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offset(-6.0F, -2.25F, 1.4749F));
+
+        PartDefinition leftArm = body.addOrReplaceChild("leftArm", CubeListBuilder.create().texOffs(32, 0).mirror().addBox(-2.0F, -0.75F, -2.5F, 4.0F, 15.0F, 4.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(6.0F, -2.25F, 0.5F));
+
+        PartDefinition cube_r3 = leftArm.addOrReplaceChild("cube_r3", CubeListBuilder.create().texOffs(13, 54).addBox(-3.0F, -2.0F, -4.0F, 4.0F, 4.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 1.0F, -2.0251F, 0.0F, 3.1416F, 0.0F));
+
+        PartDefinition rightLeg = body.addOrReplaceChild("rightLeg", CubeListBuilder.create().texOffs(32, 19).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.0F))
+                .texOffs(1, 54).addBox(-2.0F, 11.0F, -3.0F, 4.0F, 1.0F, 1.0F, new CubeDeformation(0.0F))
+                .texOffs(13, 54).addBox(-3.1F, -0.25F, -2.5251F, 4.0F, 5.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offset(-1.9F, 12.0F, 0.0F));
+
+        PartDefinition leftLeg = body.addOrReplaceChild("leftLeg", CubeListBuilder.create().texOffs(32, 19).mirror().addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.0F)).mirror(false)
+                .texOffs(1, 54).addBox(-1.8F, 11.0F, -3.0F, 4.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(1.9F, 12.0F, 0.0F));
+
+        PartDefinition cube_r4 = leftLeg.addOrReplaceChild("cube_r4", CubeListBuilder.create().texOffs(13, 54).addBox(-3.0F, -2.0F, -4.0F, 4.0F, 5.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.1F, 1.75F, -1.5251F, 0.0F, 3.1416F, 0.0F));
 
         return LayerDefinition.create(meshdefinition, 64, 64);
     }
@@ -99,21 +88,28 @@ public class NomadWarriorModel<T extends NomadWarriorEntity>
     public void setupAnim(T entity, float limbSwing, float limbSwingAmount,
                           float ageInTicks, float netHeadYaw, float headPitch) {
 
-        this.root.getAllParts().forEach(ModelPart::resetPose);
-        this.head.yRot = Mth.clamp(netHeadYaw, -30.0F, 30.0F) * ((float)Math.PI / 180F);
-        this.head.xRot = Mth.clamp(headPitch, -25.0F, 45.0F) * ((float)Math.PI / 180F);
-        float armSwing = 1.4F * limbSwingAmount;
-        this.rightArm.xRot = Mth.cos(limbSwing * 0.6662F) * armSwing;
-        this.leftArm.xRot = Mth.cos(limbSwing * 0.6662F + (float)Math.PI) * armSwing;
-        float legSwing = 1.4F * limbSwingAmount;
-        this.rightLeg.xRot = Mth.cos(limbSwing * 0.6662F) * legSwing;
-        this.leftLeg.xRot = Mth.cos(limbSwing * 0.6662F + (float)Math.PI) * legSwing;
+        this.root().getAllParts().forEach(ModelPart::resetPose);
 
-        if (entity.isAggressive()) {
-            float attackAnim = entity.getAttackAnim(0.0F); // вместо this.attackTime
-            AnimationUtils.swingWeaponDown(this.rightArm, this.leftArm, entity, attackAnim, ageInTicks);
+        this.head.yRot = netHeadYaw * ((float) Math.PI / 180F);
+        this.head.xRot = headPitch * ((float) Math.PI / 180F);
+
+        this.animateWalk(
+                NomadWarriorAnimations.NOMAD_WARRIOR_WALK,
+                limbSwing, limbSwingAmount,
+                2f, 2.5f
+        );
+
+        this.animate(
+                entity.attackAnimationState,
+                NomadWarriorAnimations.NOMAD_WARRIOR_ATTACK,
+                ageInTicks
+        );
+        if (entity.isUsingItem()) {
+            AnimationUtils.animateCrossbowHold(this.rightArm, this.leftArm, this.head, true);
         }
     }
+
+    @Override
     public ModelPart root() {
         return this.root;
     }
@@ -123,9 +119,12 @@ public class NomadWarriorModel<T extends NomadWarriorEntity>
                                int packedLight, int packedOverlay, int color) {
         root.render(poseStack, vertexConsumer, packedLight, packedOverlay, color);
     }
+
     public ModelPart getArm(HumanoidArm arm) {
         return arm == HumanoidArm.LEFT ? this.leftArm : this.rightArm;
     }
+
+
     @Override
     public void translateToHand(HumanoidArm arm, PoseStack poseStack) {
         this.getArm(arm).translateAndRotate(poseStack);
